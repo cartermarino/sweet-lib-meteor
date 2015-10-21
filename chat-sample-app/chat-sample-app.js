341a52ae3b5b10c5c8008fc49c410e5d87910dac
Joy Morgan <Tesoro1021@gmail.com>
2015-10-21 11:53:20 -0400

    Works on database creation

--- chat-sample-app/chat-sample-app.js
+++ chat-sample-app/chat-sample-app.js
@@ -1,6 +1,8 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
+  Session.setDefault('message', '')
+  Session.setDefault('messages', [])
 
   Template.hello.helpers({
     counter: function () {
@@ -12,9 +14,26 @@ if (Meteor.isClient) {
     'click button': function () {
       // increment the counter when button is clicked
       alert("clicked!")
-      // Session.set('counter', Session.get('counter') + 1);
+      Session.set('counter', Session.get('counter') + 1);
     }
   });
+
+  Template.chat_window.helpers({
+    message: function(){
+      return Session.get('messages');
+    }
+  });
+  Template.input_box.events({
+    submit: function() {
+      input_text = $("#write-chat").val()
+      Session.set('message', input_text)
+      Session.set('messages', Session.get('message'))
+
+
+      //alert("WHOAH HEY")
+    }
+  });
+
 }
 
 if (Meteor.isServer) {
@@ -22,3 +41,25 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+// Trying to set up database... not sure if works yet!
+
+// Chat = new Mongo.Collection("chat");
+// if (Meteor.isClient){
+//   Template.body.helpers({
+//     chat: function(){
+//       return Chat.find({});
+//     }
+//   });
+//   Template.body.events({
+//     "submit .new-chat": function (event){
+//       event.preventDefault();
+//       var text = event.target.text.value;
+//       Chat.insert({
+//         text: text,
+//         createdAt: new Date()
+//       });
+//       event.target.text.value = "";
+//     }
+//   })
+// }
